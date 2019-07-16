349c0e6b6f65b866cbf4e452270b31ff175e84ea
chiconunes <fcn_jr@hotmail.com>
2019-07-16 19:12:50 -0300

    agora foi

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Teste com GIT',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Testando o GIT   .'),
     );
   }
 }
