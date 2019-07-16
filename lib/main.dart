8dcba7cfa28fb1e16be65c1de2da36d68d77cc23
chiconunes <fcn_jr@hotmail.com>
2019-07-16 18:49:03 -0300

    puxando do zero !

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-/* BRINCADEIRA VAMOS TESTAR ISSO TUDO !!!!*/
-/* BRINCADEIRA VAMOS TESTAR ISSO TUDO !!!!*/
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
